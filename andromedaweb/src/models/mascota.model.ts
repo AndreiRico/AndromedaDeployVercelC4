7b6ad077af5c18cd67b954f2fae7b7b86d5c1c86
Miguelmmv <miguel3correo@gmail.com>
2022-10-26 18:19:15 -0500

    Tablas entidades sin relacion

---
+++ andromedaweb/src/models/mascota.model.ts
@@ -0,0 +1,75 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  foto?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  nacimiento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  especie: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  raza: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  sexo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  estadoAfiliacion: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaAfiliacion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  comentarioAfiliacion: string;
+
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
