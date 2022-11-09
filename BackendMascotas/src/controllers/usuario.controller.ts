7b20783dd1c7b07d470230e35c8d41fa133e9ef6
Miguelmmv <miguel3correo@gmail.com>
2022-11-09 08:43:41 -0500

    Formulario registro en Bootstrap

--- BackendMascotas/src/controllers/usuario.controller.ts
+++ BackendMascotas/src/controllers/usuario.controller.ts
@@ -66,7 +66,7 @@ export class UsuarioController {
     
   //--------------- Crear Usuarios ---------------------------
 
-  @authenticate("admin","assessor","client")
+  //@authenticate("admin","assessor","client")
 
   @post('/usuarios')
   @response(200, {
