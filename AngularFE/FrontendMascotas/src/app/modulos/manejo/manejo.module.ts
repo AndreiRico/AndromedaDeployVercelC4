06f225b5726cc058bc75631c9ef44fc7f413a847
Miguelmmv <miguel3correo@gmail.com>
2022-11-18 13:20:57 -0500

    Creados 3 modulos, seguridad, administracion y manejo

---
+++ AngularFE/FrontendMascotas/src/app/modulos/manejo/manejo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManejoRoutingModule } from './manejo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManejoRoutingModule
+  ]
+})
+export class ManejoModule { }
