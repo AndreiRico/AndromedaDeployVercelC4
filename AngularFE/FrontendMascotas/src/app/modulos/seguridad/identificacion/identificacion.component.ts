26e30233cca64b6464fddc879cdb35004439a968
Miguelmmv <miguel3correo@gmail.com>
2022-11-19 14:28:46 -0500

    Trabajando en el login, hasta guardar token

--- AngularFE/FrontendMascotas/src/app/modulos/seguridad/identificacion/identificacion.component.ts
+++ AngularFE/FrontendMascotas/src/app/modulos/seguridad/identificacion/identificacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SeguridadService } from 'src/app/servicios/seguridad.service';
 const cryptoJS = require("crypto-js")
 @Component({
   selector: 'app-identificacion',
@@ -13,16 +14,22 @@ export class IdentificacionComponent implements OnInit {
     'contrasena': ['', [Validators.required]]
   })
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private servicioSeguridad: SeguridadService) { }
   ngOnInit(): void {
   }
 
-  IdentificarUsuario(){
+  IdentificarUsuario() {
     let usuario = this.fgValidador.controls["usuario"].value;
     let contrasena = this.fgValidador.controls["contrasena"].value;
-    let contrasenaCifrada = cryptoJS.MD5(contrasena);
+    let contrasenaCifrada = cryptoJS.MD5(contrasena).toString();
     alert(usuario)
     alert(`${contrasena}\n${contrasenaCifrada}`)
+    this.servicioSeguridad.Identificar(usuario, contrasenaCifrada).subscribe((datos: any) => {
+      alert("Datos correctos!!")
+      this.servicioSeguridad.AlmacenarSesion(datos);
+    }, (error: any) => {
+      alert("Datos invalidos!!")
+    })
   }
 }
-
--- AngularFE/FrontendMascotas/src/app/servicios/seguridad.service.ts
+++ AngularFE/FrontendMascotas/src/app/servicios/seguridad.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ModeloIdentificar } from '../modelos/identificar.modelo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeguridadService {
+  
+  url = 'http://localhost:3000';
+  constructor(private http: HttpClient) { 
 
-  constructor() { }
+  }
+  Identificar(usuario: string, contrasena: string):Observable<ModeloIdentificar>{
+    return this.http.post<ModeloIdentificar>(`${this.url}/identificarUsuario`, {
+      usuario: usuario,
+      contrasena: contrasena
+    },
+    {headers: new HttpHeaders({
+
+    })})
+
+  }
+  AlmacenarSesion(datos: ModeloIdentificar){
+    let stringDatos = JSON.stringify(datos);
+    localStorage.setItem("datosSesion", stringDatos);
+  }
+
+  ObtenerInformacionSesion(){
+    let datosString = localStorage.getItem("datosSesion");
+    if(datosString){
+      let datos = JSON.parse(datosString);
+      return datos;
+    }else{
+      return null;
+    }
+  }
+
+  EliminarInformacionSesion(){
+    localStorage.removeItem("datosSesion");
+  }
 }
